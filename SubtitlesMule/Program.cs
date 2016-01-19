0c213f0a03d8da1724bf1c12ba023462a1fc9ab5
Vladimir Mašala <vladimir.masala@gmail.com>
2016-01-19 01:44:30 +0100

    Initial

---
+++ SubtitlesMule/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+namespace SubtitlesMule
+{
+    class Program
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            _logger.Trace("Hello...");
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+    }
+}
