57f1baed5abc1060f2a44dd80601c8de966f615b
Vladimir Mašala <vladimir.masala@gmail.com>
2016-01-23 20:08:21 +0100

    Fully functional  comand line version..

---
+++ SubtitlesMule/CommandLineArguments.cs
@@ -0,0 +1,45 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitlesMule
+{
+    class CommandLineArguments
+    {
+        [Option('r', "register", Required = false, HelpText = "Register File Types to process")]
+        public string RegisterFileTypes { get; set; }
+
+        [Option('f', "file", Required = true, HelpText = "Download subtitles for file")]
+        public string File { get; set; }
+
+        [Option('d', "directory", Required = false, HelpText = "Download subtitles for all files in directory (recursively)")]
+        public string Directory { get; set; }
+
+        [Option('l', "Language", Required = true, HelpText = "Language for subtitles use ISO 639-2/B Codes")]
+        public string Language { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText();
+
+            if (this.LastParserState.Errors.Any())
+            {
+                var errors = help.RenderParsingErrorsText(this, 2); // indent with two spaces
+                if (!string.IsNullOrEmpty(errors))
+                {
+                    help.AddPreOptionsLine(string.Concat(Environment.NewLine, "ERROR(S):"));
+                    help.AddPreOptionsLine(errors);
+                }
+            }
+            return help;
+        }
+    }
+}
---
+++ SubtitlesMule/SubtitleSearchItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CookComputing.XmlRpc;
+using System.Reflection;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitlesMule
+{
+    public class SubtitleSearchItem
+    {
+        //struct description http://trac.opensubtitles.org/projects/opensubtitles/wiki/XMLRPC#SearchSubtitles
+        // public string MatchedBy { get; set; }
+        //public String MovieNameEng { get; set; }
+        //public String MovieHash { get; set; }
+        //public String LanguageName { get; set; }
+        public int IDSubtitleFile { get; set; }
+        //public String SubDownloadLink { get; set; }
+        public String SubFormat { get; set; }
+        //public int SubBad { get; set; }
+        public decimal SubRating { get; set; }
+        public SubtitleSearchItem()
+        {
+
+        }
+
+        public SubtitleSearchItem(XmlRpcStruct item)
+        {
+            var reflection = this.GetType();
+
+            foreach (var key in item.Keys)
+            {
+                var propInfo = reflection.GetProperty(key.ToString(), BindingFlags.Instance | BindingFlags.Public);
+                if (propInfo != null)
+                {
+                    propInfo.SetValue(this, Convert.ChangeType(item[key], propInfo.PropertyType, CultureInfo.CurrentUICulture));
+                }
+
+            }
+        }
+    }
+}
